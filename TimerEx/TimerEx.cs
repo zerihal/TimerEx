dad0b74b4ddee6289b7bcec5a7b3f5bef30b9c86
John Sweet <zerihal@hotmail.com>
2018-09-05 00:13:01 +0100

    Add project files.

---
+++ TimerEx/TimerEx.cs
@@ -0,0 +1,19 @@
+﻿using System.Timers;
+
+namespace TimerEx
+{
+    public class TimerEx : Timer
+    {
+        public TimerEx(int v)
+        {
+            Interval = v;
+        }
+
+        public void Restart()
+        {
+            Stop();
+            Start();
+        }
+    }
+
+}

513ad9395d58bf73b52efa2eafcbd3d5443074c7
John Sweet <zerihal@hotmail.com>
2018-09-06 12:51:31 +0100

    Added XML comments and regions

--- TimerEx/TimerEx.cs
+++ TimerEx/TimerEx.cs
@@ -4,21 +4,36 @@ namespace TimerEx
 {
     public class TimerEx : Timer
     {
+        #region Construction
+        /// <summary>
+        /// Creates an instance of TimerEx with the specified interval.
+        /// </summary>
+        /// <param name="v">The interval in milliseconds until elapsed</param>
         public TimerEx(int v)
         {
             Interval = v;
         }
+        #endregion
 
+        #region Public methods
+        /// <summary>
+        /// Restarts the instance of TimerEx.
+        /// </summary>
         public void Restart()
         {
             Stop();
             Start();
         }
 
+        /// <summary>
+        /// Returns the number of ticks (times that the TimerEx has elapsed) since it was created.
+        /// </summary>
+        /// <returns>Number of ticks or null</returns>
         public int? GetTicks()
         {
             return null;
         }
+        #endregion
     }
 
 }
